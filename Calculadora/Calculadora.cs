88ad54f926d022beed3bac5600ce3ee7c07d2d25
Julian <jc6070060@gmail.com>
2026-05-26 13:05:06 -0300

    falta terminar punto 1

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,29 @@
+using System.Data;
+
+namespace EspacioCalculadora;
+
+public class Calculadora
+{
+    private double dato;
+    public double Dato {get => dato;}
+    public void Sumar(double termino)
+    {
+        dato = dato + termino;
+    }
+    public void Restar(double termino)
+    {
+        dato = dato - termino;
+    }
+    public void Multiplicar(double termino)
+    {
+        dato = dato * termino;
+    }
+    public void Dividir(double termino)
+    {
+        dato = dato / termino;
+    }
+    public void Limpiar()
+    {
+        dato = 0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using EspacioCalculadora;
+
+Console.WriteLine("Hello, World!");
+
+Calculadora miCalculadora = new Calculadora();
+miCalculadora.Limpiar();
+miCalculadora.Sumar(5);
+Console.WriteLine("Resultado: " + miCalculadora.Dato);
+bool continuar = true;
+
+void operar(int operacion)
+{
+    
+}
+
+while(continuar)
+{
+    Console.WriteLine("1- Sumar");
+    Console.WriteLine("2- Restar");
+    Console.WriteLine("3- Multiplicar");
+    Console.WriteLine("4- Dividir");
+    Console.WriteLine("5- Limpiar");
+    Console.WriteLine("0- Salir");
+    string opcion = Console.ReadLine();
+    switch(opcion)
+    {
+        case "1":
+            Console.WriteLine("Ingrese el numero a sumar");
+            int num;
+            string numeroIngresado;
+            numeroIngresado = Console.ReadLine();
+            bool resultado = int.TryParse(numeroIngresado, out num);
+            miCalculadora.Sumar(num);
+            break;
+        case "2":
+            Console.WriteLine("Ingrese el numero a restar");
+            break;
+    }
+}
