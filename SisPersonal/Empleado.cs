e8ad353b0d961a5e57ad43b9b20738899611ea2e
Julian <jc6070060@gmail.com>
2026-06-02 13:46:50 -0300

    Cambios punto 2

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,20 +1,20 @@
 public class Empleado
 {
-    private string Nombre;
-    private string Apellido;
-    private DateTime FechaDeNacimiento;
-    private char EstadoCivil;
-    private DateTime FechaIngresoEmpresa;
-    private double Sueldo;
+    private string nombre;
+    private string apellido;
+    private DateTime fechaDeNacimiento;
+    private char estadoCivil;
+    private DateTime fechaIngresoEmpresa;
+    private double sueldo;
 
-    public string nombre { get => Nombre; set => Nombre = value; }
-    public string apellido { get => Apellido; set => Apellido = value; }
-    public DateTime fechaNacimiento { get => FechaDeNacimiento; set => FechaDeNacimiento = value; }
-    public char estadoCivil { get => EstadoCivil; set => EstadoCivil = value; }
-    public DateTime fechaIngresoEmpresa { get => FechaIngresoEmpresa; set => FechaIngresoEmpresa = value; }
-    public double sueldo { get => Sueldo; set => Sueldo = value; }
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Apellido { get => apellido; set => apellido = value; }
+    public DateTime FechaDeNacimiento { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
+    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+    public DateTime FechaIngresoEmpresa { get => fechaIngresoEmpresa; set => fechaIngresoEmpresa = value; }
+    public double Sueldo { get => sueldo; set => sueldo = value; }
 
-    enum cargos
+    enum Cargos
     {
         Auxiliar = 1,
         Administrativo = 2,
@@ -25,15 +25,15 @@ public class Empleado
 
     public int CalcularAntiguedad()
     {
-        return DateTime.Now.Year - fechaIngresoEmpresa.Year;
+        return DateTime.Now.Year - FechaIngresoEmpresa.Year;
     }
     public int CalcularAniosEmpleado()
     {
-        return DateTime.Now.Year - fechaNacimiento.Year;
+        return DateTime.Now.Year - fechaDeNacimiento.Year;
     }
     public int CalcularAniosJubilacion()
     {
-        int edadEmpleado = DateTime.Now.Year - fechaNacimiento.Year;
+        int edadEmpleado = DateTime.Now.Year - fechaDeNacimiento.Year;
         if (edadEmpleado <= 65)
         {
             return 65 - edadEmpleado;
