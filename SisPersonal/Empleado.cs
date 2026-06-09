8d273031d1022f6bad3e623e17ba0961e407e57b
Julian <jc6070060@gmail.com>
2026-06-09 13:10:06 -0300

    Empleado ya completado

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,3 +1,4 @@
+namespace EmpleadoEmpresa;
 public class Empleado
 {
     private string nombre;
@@ -6,22 +7,16 @@ public class Empleado
     private char estadoCivil;
     private DateTime fechaIngresoEmpresa;
     private double sueldo;
-
+    private Cargos cargo;
     public string Nombre { get => nombre; set => nombre = value; }
     public string Apellido { get => apellido; set => apellido = value; }
     public DateTime FechaDeNacimiento { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
     public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
     public DateTime FechaIngresoEmpresa { get => fechaIngresoEmpresa; set => fechaIngresoEmpresa = value; }
     public double Sueldo { get => sueldo; set => sueldo = value; }
+    public Cargos Cargo { get => cargo; set => cargo = value; }
 
-    enum Cargos
-    {
-        Auxiliar = 1,
-        Administrativo = 2,
-        Ingeniero = 3,
-        Especialista = 4,
-        Investigador = 5,
-    }
+
 
     public int CalcularAntiguedad()
     {
@@ -37,9 +32,44 @@ public class Empleado
         if (edadEmpleado <= 65)
         {
             return 65 - edadEmpleado;
-        } else
+        }
+        else
         {
             return -1;
         }
     }
+    public double Adicional()
+    {
+        double adicional = 0;
+        double Salario;
+                if (CalcularAntiguedad() <= 20)
+                {
+                    adicional = Sueldo *  (CalcularAntiguedad() * 0.01);
+                }
+                else
+                {
+                    adicional = Sueldo * 0.25;
+                }
+
+                if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+                {
+                    adicional *= 1.50;
+                }
+                
+                if (EstadoCivil == 'C' || EstadoCivil == 'c')
+                {
+                    adicional += 150000;
+                }
+            Salario = sueldo + adicional;
+        return Salario;
+    }
+}
+
+public enum Cargos
+{
+    Auxiliar = 1,
+    Administrativo = 2,
+    Ingeniero = 3,
+    Especialista = 4,
+    Investigador = 5,
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EmpleadoEmpresa;
+
+Empleado[] empleados = new Empleado[3];
+
+for (int i = 0; i < 3; i++)
+{
+    empleados[i] = new Empleado();
+    empleados[i].Nombre = Console.ReadLine();
+    empleados[i].Apellido = Console.ReadLine();
+    
+}
